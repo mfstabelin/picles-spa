4bc3a7be65dc03d3e6bdd5408ba4761b78bfceed
mfstabelina <matheus.stabelin@hotmail.com.br>
2024-04-16 22:22:37 -0300

    implentado botoes pagina inicial, pagina de pet e listagem de pets

---
+++ src/components/layout/Grid/Grid.tsx
@@ -0,0 +1,11 @@
+
+import { ReactNode } from 'react'
+import styles from './Grid.module.css'
+
+interface IGrid {
+    children: ReactNode
+}
+
+export function Grid({ children }: IGrid) {
+    return <div className={styles.grid}>{children}</div>
+}
